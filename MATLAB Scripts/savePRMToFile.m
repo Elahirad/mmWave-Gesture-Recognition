cbcf8f718286912611d7210a57f6eef7011be809
NYXTheShadow <alielahirad8087@gmail.com>
2024-08-26 15:15:25 +0330

    Added PRM calculation and save to MATLAB scripts.

---
+++ MATLAB Scripts/savePRMToFile.m
@@ -0,0 +1,15 @@
+function savePRMToFile(PRMStat, filename)
+
+    if exist(filename, 'file')
+        fid = fopen(filename, 'a'); % Append data
+    else
+        fid = fopen(filename, 'w'); % Create new file and write header
+        fprintf(fid, 'NEC, REC, VEC, NCP, RCP, VCP, NCF, RCF, VCF, nearest_alpha\n');
+    end
+    
+    fprintf(fid, '%d, %.6f, %.6f, %d, %.6f, %.6f, %d, %.6f, %.6f, %.6f\n', ...
+            PRMStat(1), PRMStat(2), PRMStat(3), PRMStat(4), ...
+            PRMStat(5), PRMStat(6), PRMStat(7), PRMStat(8), ...
+            PRMStat(9), PRMStat(10));
+
+end
